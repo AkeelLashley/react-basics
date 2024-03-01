be3486ce938e2bbf00a26f3d97f6038cbbf4ee7c
Akeel Lashley <akeel@futuretek.ca>
2024-03-01 10:27:42 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//A function which returns html
+import Greeting from "./components/Greeting";
+
+const App = () => {
+  return (
+    // React Fragment (<> </>)
+    // Create a component named "Image.tsx" which returns an image of a cat. Call this component in the App.tsx
+    <>
+      <Greeting />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,13 @@
+import "./Greeting.css";
+
+const Greeting = () => {
+  return (
+    <>
+      <h1 className="heading">Welcome to my app</h1>
+      <p>my name is Akeel</p>
+      <button>Click me</button>
+    </>
+  );
+};
+
+export default Greeting;
